5cbdf42da743e927bf99b7de62c060779fa510ea
Tarsis <br.tmvdl@gmail.com>
2023-11-21 11:32:04 -0300

    Adding app files

---
+++ src/app/utils/colors.js
@@ -0,0 +1,8 @@
+export const BLACK_1 = '#000000'
+export const BLACK_2 = '#333333'
+
+export const GRAY_1 = '#666666'
+export const GRAY_2 = '#999999'
+
+export const WHITE_1 = '#ffffff'
+export const WHITE_2 = '#cccccc'
